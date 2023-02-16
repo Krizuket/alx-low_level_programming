3710be191e530111fbfc24fd1248ed98ed502e39
kriz uket <krisuket@gmail.com>
2023-02-16 10:47:00 -0800

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/**
+ * main -A program thatprint a line with puts function
+ * Return: 0 (Success)
+ */int main(void)
+{puts("\"Programming is like building a multilingual puzzle"); return (0);}
