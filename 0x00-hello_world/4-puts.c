cd81645c7b6560080bb4f6b370cca3142e1af7f2
kriz uket <krisuket@gmail.com>
2023-02-19 14:17:36 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function to print a string to the stout starts here
+ *
+ * Return: when 0 (no error)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
