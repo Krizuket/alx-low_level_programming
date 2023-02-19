e13b5387d45a66cbc71249b1e01452188d009dde
kriz uket <krisuket@gmail.com>
2023-02-19 11:27:43 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* imported the librabries I need to run the program */
+
+/**
+* main - program function starts here
+*
+* Return: 0 (when no error)
+*/
+int main(void)
+{
+
+int n;
+int lastDigit;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
+/* conditional statements starts here */
+
+if (lastDigit > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+}
+else if (lastDigit == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+}
+else if (lastDigit < 6)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+}
+return (0);
+}
