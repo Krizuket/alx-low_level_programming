39a77ad1dd3b7ba867b1b22f98a36aa09a073615
kriz uket <krisuket@gmail.com>
2023-02-19 12:34:23 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* imported the librabries I need to run the program */
+
+/**
+* main - program function starts here
+*
+* Return: 0 (when no error)
+*/
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
+}
