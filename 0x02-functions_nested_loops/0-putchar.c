f70c71f997b6b84f968f05c89d5486544ba0472c
kriz uket <krisuket@gmail.com>
2023-02-22 15:15:46 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* imported the main.h library I need to run the program */
+
+/**
+* main - Entry point 
+*
+* Return: 0 (when no error)
+*/
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+}
