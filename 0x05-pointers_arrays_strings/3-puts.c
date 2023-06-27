569dce3edffcd15fee8035ee476f838129498464
root <krisuket@gmail.com>
2023-06-27 03:47:55 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,10 +5,10 @@
  * @str: string to print
  */
 void _puts(char *str)
-}
-while (*str != '\0')
 {
-	_putchar(*str++);
-}
-_putchar('\n');
+    while (*str != '\0')
+    {
+        _putchar(*str++);
+    }
+    _putchar('\n');
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,22 +5,21 @@
  * @s: string
  * return: 0
  */
-void print_rev(char *s);
+void print_rev(char *s)
 {
-	int longi = 0;
-	int o;
+    int longi = 0;
+    int o;
 
-	while (*s != '\0')
-	{
-	 longi++;
-	 s++;
-	}
-	s--;
-	for (o = longi; o > 0; o--)
-	{
-		_putchar(*s);
-		s--;
-	}
+    while (*s != '\0')
+    {
+        longi++;
+        s++;
+    }
+    s--;
 
-	_putchar('\n');
+    for (o = longi - 1; o >= 0; o--)
+    {
+        _putchar(*s);
+        s--;
+    }
 }
